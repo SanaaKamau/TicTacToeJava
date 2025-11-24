d326528114d51f9438b4c422a2a5b4f6fb7a3eb7
SanaaKamau <98336407+SanaaKamau@users.noreply.github.com>
2025-11-24 15:42:31 -0500

    Added start, player, round, game, gameover scene

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class Main extends Application {
+    private Scene startScreen;
+    private Scene playerPickScreen;
+    private Scene roundPickScreen;
+    private Scene gameScreen;
+    private Scene gameOverScreen;
+
+    public void init(){
+        
+    }
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("JavaFX Window");
+        stage.setTitle("TicTacToe Start");
         stage.show();
         
         
@@ -14,6 +24,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-       
+        
     }
 }
